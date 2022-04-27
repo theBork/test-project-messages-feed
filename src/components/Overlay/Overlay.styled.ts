b1d046a52ce91da064a998029099c81fcfec1c4d
thebork <m.kutnyashenko@gmail.com>
2022-04-27 21:49:36 +0300

    Created common Overlay component

---
+++ src/components/Overlay/Overlay.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import {zIndex} from "styled-system";
+import Box from "ui/Box";
+
+export const Overlay = styled(Box).attrs({
+  bg: "overlay40",
+  zIndex: "overlay",
+})`
+  ${zIndex};
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+`;
