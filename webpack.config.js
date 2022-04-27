b7b2094ee18021c6fa29e7d6644de478a193fbd0
thebork <m.kutnyashenko@gmail.com>
2022-04-27 20:42:56 +0300

    Added project template with minimum required libraries and webpack/babel/eslint configs

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: path.resolve(process.cwd(), "src", "index.tsx"),
+  mode: "development",
+  output: {
+    path: path.resolve(process.cwd(), "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: path.resolve(process.cwd(), "node_modules"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ]
+          },
+        }
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              svgrConfig: {
+                plugins: {
+                  removeViewBox: false,
+                }
+              }
+            }
+          },
+          "file-loader"
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+        ]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "url-loader",
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(process.cwd(), 'public'),
+    },
+    compress: true,
+    port: 3000,
+  },
+}
